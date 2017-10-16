1a0e27241993a7c70bd8c6e485ad638158917654
abdussakur <32808818+abdussakur@users.noreply.github.com>
2017-10-16 07:29:57 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+	<title>TUGAS FRAMEWORK 1</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="container-fluid">
+<div class="row">
+<div class="col-sm-3 col-1">
+<h4>KOLOM 1</h4>
+	</div>
+<div class="col-sm-3 col-2">
+<h4>KOLOM 2</h4>
+	</div>
+<div class="col-sm-3 col-3">
+<h4>KOLOM 3</h4>
+	</div>
+	<div class="col-sm-3 col-4">
+<h4>KOLOM 4</h4>
+	</div>
+	<div class="col-sm-3 col-5">
+<h4>KOLOM 5</h4>
+	</div>
+	<div class="col-sm-3 col-6">
+<h4>KOLOM 6</h4>
+	</div>
+<div class="col-sm-3 col-7">
+<h4>KOLOM 7</h4>
+	</div>
+<div class="col-sm-3 col-8">
+<h4>KOLOM 8</h4>
+	</div>
+<div class="col-sm-3 col-9">
+<h4>KOLOM 9</h4>
+	</div>
+<div class="col-sm-3 col-10">
+<h4>KOLOM 10</h4>
+	</div>
+<div class="col-sm-3 col-11">
+<h4>KOLOM 11</h4>
+	</div>
+<div class="col-sm-3 col-12">
+<h4>KOLOM 12</h4>
+	</div>
+</div>
+</div>
+<style> 
+.col-1{
+	background-color: #990033;
+}
+.col-2{
+	background-color: #990066;
+}
+.col-3{
+	background-color: #990033;
+}
+.col-4{
+	background-color: #990066;
+}
+.col-5{
+	background-color: #FFFF33;
+}
+.col-6{
+	background-color: #e74c3c;
+}
+.col-7{
+	background-color: #3498db;
+}
+.col-8{
+	background-color: #2ecc71;
+}
+.col-9{
+	background-color: #99FFCC;
+}
+.col-10{
+	background-color: #FF66FF;
+}
+.col-11{
+	background-color: #99FFCC;
+}
+.col-12{
+	background-color: #FF66FF;
+}
+</style>
+</body>
+</html>
